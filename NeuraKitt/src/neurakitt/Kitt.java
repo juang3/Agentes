5095a53e2d99851626e03b6529fd1a0ee7041681
Álvaro López <alopezj97@gmail.com>
2018-10-27 14:38:25 +0200

    Hecha la historia de usuario 3

--- NeuraKitt/src/neurakitt/Kitt.java
+++ NeuraKitt/src/neurakitt/Kitt.java
@@ -67,29 +67,85 @@ public class Kitt extends Agente {
             Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        /* Si nos hemos logeado correctamente, guardamos la clave y escuchamos a neura con la siguiente acción a realizar */
-        
         mensaje = Json.parse(respuesta.getContent()).asObject();
+        
+        /* Si nos hemos logeado correctamente, guardamos la clave, recibimos del servidor la batería y determinamos la acción a llevar a cabo*/
+        
         if ( !mensaje.get("result").asString().equals("BAD_MAP") && !mensaje.get("result").asString().equals("BAD_PROTOCOL") ) {
             String clave = mensaje.get("result").asString();    
+            
+            /* Escuchamos al servidor para recibir la batería */
+            
             try {
-                /* Escuchamos a neura para que nos diga la acción a realizar */
                 respuesta = this.receiveACLMessage();
             } catch (InterruptedException ex) {
                 System.err.println("Error al recibir la respuesta");
                 Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
             }
             mensaje = Json.parse(respuesta.getContent()).asObject();
+            double bateria = mensaje.get("battery").asDouble() ;
+            
+            /* Escuchamos a neura para recibir la acción a realizar */
+
+            try {
+                respuesta = this.receiveACLMessage();
+            } catch (InterruptedException ex) {
+                System.err.println("Error al recibir la respuesta");
+                Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            mensaje = Json.parse(respuesta.getContent()).asObject();            
                 
             /* Mientras que la acción a realizar no sea la de hacer logout, determinamos qué acción es, la llevamos a cabo 
                y volvemos a escuchar a neura (TO DO - INCOMPLETO) */
             
             while ( !mensaje.get("accion").equals("logout") ) {
 
-                // RELLENAR AQUÍ
+                /* Si no me queda batería hago refuel */
+                
+                if (bateria == 0) {
+                   
+                    /* Le enviamos al servidor el mensaje refuel */
+            
+                    outbox = new ACLMessage();
+                    outbox.setSender(this.getAid());
+                    outbox.setReceiver(idServidor); // establecemos destinatario
+
+                    /* Creamos el mensaje */
+
+                    mensaje = new JsonObject();
+                    mensaje.add("command", "refuel");
+                    mensaje.add("key", clave);
+                    outbox.setContent(mensaje.asString());
+
+                    this.send(outbox); // lo enviamos
                     
+                    /* Recibimos la respuesta del servidor */
+                    
+                    try {
+                        respuesta = this.receiveACLMessage();
+                    } catch (InterruptedException ex) {
+                        System.err.println("Error al recibir la respuesta");
+                        Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    mensaje = Json.parse(respuesta.getContent()).asObject();            
+                    
+                    if (!mensaje.get("result").asString().equals("OK"))
+                        System.err.println("Error al hacer refuel");
+                  
+                }
+                
+                /* Realizo la acción que me diga neura */
+                
+                else {
+
+                    // RELLENAR AQUÍ
+                    
+                }
+                
+                /* Escuchamos de nuevo a neura */
+                
                 try {
-                    respuesta = this.receiveACLMessage(); // Escuchamos de nuevo a neura
+                    respuesta = this.receiveACLMessage(); 
                 } catch (InterruptedException ex) {
                     System.err.println("Error al recibir la respuesta");
                     Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
@@ -114,6 +170,7 @@ public class Kitt extends Agente {
             this.send(outbox); // lo enviamos
 
             /* Recibimos la respuesta del servidor y si el resultado es OK guardamos la traza */
+            
             try {
                 respuesta = this.receiveACLMessage();
                 mensaje = Json.parse(respuesta.getContent()).asObject();
