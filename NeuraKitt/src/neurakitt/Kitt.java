00d5493bdf204cb91faa829d44f7c957cbb79f1f
Álvaro López <alopezj97@gmail.com>
2018-10-31 11:43:07 +0100

    Corregidos errores, modularizadas funciones

--- NeuraKitt/src/neurakitt/Kitt.java
+++ NeuraKitt/src/neurakitt/Kitt.java
@@ -24,6 +24,12 @@ import java.util.logging.Logger;
  */
 public class Kitt extends Agente {
     
+    
+    private AgentID idServidor ;
+    private String clave ;
+    private float bateria ;
+    
+    
     /**
      * @author Alvaro
      * @param aid
@@ -38,68 +44,30 @@ public class Kitt extends Agente {
      * @author Alvaro
      */
     public void execute() {
-        System.out.println("Hola, soy Kitt");
         
-        /* Enviamos mensaje de login al servidor */
+        //System.out.println("Hola, soy Kitt");
         
-        ACLMessage outbox = new ACLMessage(); // mensaje a enviar
-        outbox.setSender(this.getAid()); // establecemos emisor
-        AgentID idServidor = new AgentID("Girtab"); // el agente del servidor es Geminis o Girtab?
-        outbox.setReceiver(idServidor); // establecemos destinatario
+        mensaje = login();  // Recibimos el mensaje al logearnos
         
-        /* Creamos el mensaje */
-        
-        JsonObject mensaje = new JsonObject(); 
-        mensaje.add("command", "login");
-        mensaje.add("world", "map1");
-        mensaje.add("battery", "kitt");
-        mensaje.add("radar", "neura");
-        mensaje.add("scanner", "neura");
-        mensaje.add("gps", "neura");        
-        outbox.setContent(mensaje.toString());
-        
-        this.send(outbox); // lo enviamos
-        
-        /* Recibimos la respuesta del servidor */
-        
-        ACLMessage respuesta = null;
-        try {
-            respuesta = this.receiveACLMessage();
-            System.out.println("Respuesta: "+ respuesta.getContent());
-        } catch (InterruptedException ex) {
-            System.err.println("Error al recibir la respuesta");
-            Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        mensaje = Json.parse(respuesta.getContent()).asObject();
-        System.out.println("respuesta = " + mensaje.get("trace").asString());
-        System.out.println("aqui");
+        //System.out.println("respuesta = " + mensaje.get("trace").asString());
+        System.out.println("Logeado correctamente");
+        //System.out.println("Respuesta: "+ mensaje_respuesta.getContent());
+
         /* Si nos hemos logeado correctamente, guardamos la clave, recibimos del servidor la batería y determinamos la acción a llevar a cabo*/
         
         if ( !mensaje.get("result").asString().equals("BAD_MAP") && !mensaje.get("result").asString().equals("BAD_PROTOCOL") ) {
-            String clave = mensaje.get("result").asString();
-            System.out.println("ok");
+            clave = mensaje.get("result").asString();
+            System.out.println("Clave almacenada correctamente");
             
             /* Escuchamos al servidor para recibir la batería */
             
-            try {
-                respuesta = this.receiveACLMessage();
-            } catch (InterruptedException ex) {
-                System.err.println("Error al recibir la respuesta");
-                Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            mensaje = Json.parse(respuesta.getContent()).asObject();
-            double bateria = mensaje.get("battery").asDouble() ;
+            ReciboYDecodificoMensaje();
+            System.out.println("Recibimos bateria del servidor: "+ mensaje_respuesta.getContent());
+            bateria = mensaje.get("battery").asFloat() ;
             
             /* Escuchamos a neura para recibir la acción a realizar */
 
-            try {
-                respuesta = this.receiveACLMessage();
-            } catch (InterruptedException ex) {
-                System.err.println("Error al recibir la respuesta");
-                Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            mensaje = Json.parse(respuesta.getContent()).asObject();            
+            ReciboYDecodificoMensaje();
                 
             /* Mientras que la acción a realizar no sea la de hacer logout, determinamos qué acción es, la llevamos a cabo 
                y volvemos a escuchar a neura (TO DO - INCOMPLETO) */
@@ -111,29 +79,18 @@ public class Kitt extends Agente {
                 if (bateria == 0) {
                    
                     /* Le enviamos al servidor el mensaje refuel */
-            
-                    outbox = new ACLMessage();
-                    outbox.setSender(this.getAid());
-                    outbox.setReceiver(idServidor); // establecemos destinatario
-
                     /* Creamos el mensaje */
 
                     mensaje = new JsonObject();
                     mensaje.add("command", "refuel");
                     mensaje.add("key", clave);
-                    outbox.setContent(mensaje.asString());
-
-                    this.send(outbox); // lo enviamos
-                    
+                    mensaje_respuesta.setContent(mensaje.asString());
+            
+                    CodificoYEnvioMensaje(idServidor);
+                                        
                     /* Recibimos la respuesta del servidor */
                     
-                    try {
-                        respuesta = this.receiveACLMessage();
-                    } catch (InterruptedException ex) {
-                        System.err.println("Error al recibir la respuesta");
-                        Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    mensaje = Json.parse(respuesta.getContent()).asObject();            
+                    ReciboYDecodificoMensaje();
                     
                     if (!mensaje.get("result").asString().equals("OK"))
                         System.err.println("Error al hacer refuel");
@@ -150,62 +107,100 @@ public class Kitt extends Agente {
                 
                 /* Escuchamos de nuevo a neura */
                 
-                try {
-                    respuesta = this.receiveACLMessage(); 
-                } catch (InterruptedException ex) {
-                    System.err.println("Error al recibir la respuesta");
-                    Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                mensaje = Json.parse(respuesta.getContent()).asObject();
-                    
+                ReciboYDecodificoMensaje(); 
+                
             }
   
             /* Si hemos recibido un logout como acción de neura se lo enviamos al servidor */
+            logout();          
             
-            outbox = new ACLMessage();
-            outbox.setSender(this.getAid());
-            outbox.setReceiver(idServidor); // establecemos destinatario
-
-            /* Creamos el mensaje */
-            
-            mensaje = new JsonObject();
-            mensaje.add("command", "logout");
-            mensaje.add("key", clave);
-            outbox.setContent(mensaje.asString());
-
-            this.send(outbox); // lo enviamos
-
-            /* Recibimos la respuesta del servidor y si el resultado es OK guardamos la traza */
-            
-            try {
-                respuesta = this.receiveACLMessage();
-                mensaje = Json.parse(respuesta.getContent()).asObject();
-                if (mensaje.get("result").asString().equals("OK")) {
-                
-                    System.out.println("Recibiendo traza");
-                    JsonArray ja = mensaje.get("trace").asArray();
-                byte data[] = new byte [ja.size()];
-                for (int i = 0 ; i < data.length; i++) {
-                    data[i] = (byte) ja.get(i).asInt();
-                }
-                FileOutputStream fos = new FileOutputStream("mitraza.png");
-                fos.write(data);
-                fos.close();
-                
-                System.out.println("Traza Guardada");
-            }
-                
-            } catch (InterruptedException | IOException ex) {
-                System.err.println("Error al recibir la respuesta o al crear la salida con la traza");
-                Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-          
         }else{
             System.out.println("MAL");
         }
            
         
+    }
+    
+    /**
+     * @author Alvaro
+     */
+    private JsonObject login() {
+        
+        idServidor = new AgentID("Girtab"); // el agente del servidor es Geminis o Girtab?
+        
+        /* Creamos el mensaje */
+        
+        mensaje = new JsonObject(); 
+        mensaje.add("command", "login");
+        mensaje.add("world", "map1");
+        mensaje.add("battery", "kitt");
+        mensaje.add("radar", "neura");
+        mensaje.add("scanner", "neura");
+        mensaje.add("gps", "neura");        
+        
+        CodificoYEnvioMensaje(idServidor);
+        
+        this.send(mensaje_salida); // lo enviamos                   
+        
+        /* Recibimos la respuesta del servidor */
+        
+        ReciboYDecodificoMensaje();
+        System.out.println("Respuesta: "+ mensaje_respuesta.getContent());
+
+        if (mensaje.get("trace").isTrue()) {
+            /* Hacemos logout */
+            System.out.println("Llamamos a logout");
+            logout();
+            System.out.println("Despues del logout");
+            
+        }
+        
+        ReciboYDecodificoMensaje();
+        System.out.println("Respuesta: "+ mensaje_respuesta.getContent());
+        
+        return mensaje ;
+        
+    }
+    
+    /**
+     * @author Alvaro
+     */
+    private void logout() {
+
+        /* Creamos el mensaje */
+        
+        mensaje = new JsonObject();
+        mensaje.add("command", "logout");
+        mensaje.add("key", clave);
+        mensaje_salida.setContent(mensaje.asString());
+        
+        CodificoYEnvioMensaje(idServidor);
+
+        /* Recibimos la respuesta del servidor y si el resultado es OK guardamos la traza */
+        
+        try {
+            mensaje_respuesta = this.receiveACLMessage();
+            mensaje = Json.parse(mensaje_respuesta.getContent()).asObject();
+            if (mensaje.get("result").asString().equals("OK")) {
+
+                System.out.println("Recibiendo traza");
+                JsonArray ja = mensaje.get("trace").asArray();
+            byte data[] = new byte [ja.size()];
+            for (int i = 0 ; i < data.length; i++) {
+                data[i] = (byte) ja.get(i).asInt();
+            }
+            FileOutputStream fos = new FileOutputStream("mitraza.png");
+            fos.write(data);
+            fos.close();
+
+            System.out.println("Traza Guardada");
+        }
+
+        } catch (InterruptedException | IOException ex) {
+            System.err.println("Error al recibir la respuesta o al crear la salida con la traza");
+            Logger.getLogger(Kitt.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
     
 }
